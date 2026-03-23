ba0e8be53a002d89a8f28e1550929b71f57416b5
Ibrahim <s1123040@uomustansiriyah.edu.iq>
2026-03-24 01:39:38 +0300

    Add src/tools/index.ts

---
+++ src/tools/index.ts
@@ -0,0 +1,8 @@
+export { BaseTool, type ToolResult, type RiskLevel, type ToolSchema } from "./base-tool.js";
+export { ToolRegistry, toolRegistry } from "./tool-registry.js";
+export { RunTerminalTool } from "./run-terminal.js";
+export { ReadFileTool } from "./read-file.js";
+export { WriteFileTool } from "./write-file.js";
+export { OpenAppTool } from "./open-app.js";
+export { SearchWebTool } from "./search-web.js";
+export { loadTools } from "./load-tools.js";
